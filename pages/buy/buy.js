fcacf8020cc17bb15e8aaa42f1cd7c088614e64a
Kong Zheng <kongzheng1993@hotmail.com>
2019-01-04 09:54:55 +0800

    add bet,buy,buyed

--- pages/buy/buy.js
+++ pages/buy/buy.js
@@ -5,62 +5,92 @@ Page({
    * 页面的初始数据
    */
   data: {
+    betSites: ["翠福园投注站", "常营投注站", "东坝投注站"],
+    betSiteIndex: 0,
+    betTypeList: [{
+      "picName": "shuangseqiu",
+      "name": "双色球",
+      "id": 0
+    }, {
+      "picName": "qilecai",
+      "name": "七乐彩",
+      "id": 1
+    }, {
+      "picName": "3D",
+      "name": "3D",
+      "id": 2
+    }]
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  bindBetSiteChange: function(e) {
+    console.log('picker betSite 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      betSiteIndex: e.detail.value
+    })
+  },
+
+  clickBetTypeBtn: function(e) {
+    console.log("betType-id: " + e.currentTarget.dataset.betTypeId)
+    var betTypeId = e.currentTarget.dataset.betTypeId;
+    wx.navigateTo({
+      url: '/pages/bet/bet?betTypeId=' + betTypeId
+    })
   }
 })
--- pages/buyed/buyed.js
+++ pages/buyed/buyed.js
@@ -5,62 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
+    buyedList: [{
+      "buyedId": "1000000001",
+      "buyedType": "七乐彩",
+      "buyedBetSite": "翠福园投注站",
+      "buyedNums": "1,12,15,19,20,31-06"
+    }, {
+      "buyedId": "1000000001",
+      "buyedType": "双色球",
+      "buyedBetSite": "草房投注站",
+      "buyedNums": "1,12,15,19,20,31-06"
+    }, {
+      "buyedId": "1000000001",
+      "buyedType": "3D",
+      "buyedBetSite": "常营投注站",
+      "buyedNums": "1,12,15,19,20,31-06"
+    }]
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
